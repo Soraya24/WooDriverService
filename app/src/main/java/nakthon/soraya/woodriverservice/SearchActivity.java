8d522f6efd0325d1c01de9f132a02fb21b6e5189
masterUNG <phrombutr@gmail.com>
2017-06-08 09:19:51 +0700

    Start 8June

--- app/src/main/java/nakthon/soraya/woodriverservice/SearchActivity.java
+++ app/src/main/java/nakthon/soraya/woodriverservice/SearchActivity.java
@@ -2,12 +2,28 @@ package nakthon.soraya.woodriverservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        //Initial View
+        initialView();
+
+
     }   // Main Method
+
+    private void initialView() {
+        editText = (EditText) findViewById(R.id.edtSearch);
+        listView = (ListView) findViewById(R.id.livLocation);
+    }
+
 }   // Main Class
